b5b01564fd18ced8dfa53134fd4c50a715837b48
aurelioo29 <aureliolucio5@gmail.com>
2025-09-22 03:08:18 +0700

    feat: add tes labotoriyn pages with responsive (unfinish)

---
+++ app/[locale]/(main)/tes-laboratorium/page.js
@@ -0,0 +1,10 @@
+import TesLaboratoriumHero from "@/components/TesLaboratoriumHero";
+import React from "react";
+
+export default function TesLaboratoriumPage() {
+  return (
+    <>
+      <TesLaboratoriumHero />
+    </>
+  );
+}
