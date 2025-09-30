da1bcf0128d37b0f1487fe5939ae1b8872e24897
aurelioo29 <aureliolucio5@gmail.com>
2025-09-30 15:43:09 +0700

    feat: add investor hero pages

---
+++ app/[locale]/(main)/hubungan-investor/page.js
@@ -0,0 +1,10 @@
+import InverstorHero from "@/components/InverstorHero";
+import React from "react";
+
+export default function HubunganInvestorPage() {
+  return (
+    <div>
+      <InverstorHero />
+    </div>
+  );
+}
