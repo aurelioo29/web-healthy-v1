712c285b2c7df446f6935cafa907db6187787a79
aurelioo29 <aureliolucio5@gmail.com>
2025-10-05 19:06:24 +0700

    feat: add layanan-klinik client side integrate with backend

--- app/[locale]/(main)/layanan-klinik/page.js
+++ app/[locale]/(main)/layanan-klinik/page.js
@@ -1,18 +1,12 @@
 import LayananHero from "@/components/LayananHero";
-import PersonalCheck from "@/components/PersonalCheck";
-import TesLaboratorium from "@/components/TesLaboratorium";
-import VaccineSection from "@/components/VaccineSection";
-import VitaminSection from "@/components/VitaminSection";
 import React from "react";
+import LayananKlinik from "@/components/LayananKlinik";
 
 export default function LayananKlinikPage() {
   return (
     <>
       <LayananHero />
-      <VaccineSection />
-      <VitaminSection />
-      <PersonalCheck />
-      <TesLaboratorium />
+      <LayananKlinik tNs="vitamin" />
     </>
   );
 }
